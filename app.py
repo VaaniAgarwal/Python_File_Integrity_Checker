160c2cfb8d517a1000a58b7375c771f232ad0337
Vaani Agarwal <vaaniagarwal0307@gmail.com>
2025-03-23 23:33:32 +0530

    Added app.py and index.html

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask,render_template
+import os
+from flask import Flask,render_template,request,redirect,url_for,flash
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.secret_key='secret_key_of_the_person'
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 @app.route('/')
 def home():
-    return "Welcome to Python File Integrity Checker!"
-
+    return render_template('index.html')
+@app.route('/upload',methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        flash('Oops! No file was selected. Please choose a file to upload.')
+        return redirect(url_for('home'))
+    file = request.files['file']
+    if file.filename=='':
+        flash('No file selected')
+        return redirect(url_for('home'))
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    flash('file uploaded successfully')
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True)
